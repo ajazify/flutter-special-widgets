f9887327ef4dac21cc2463b22390bf2033056ad5
Muhammed Ajas A A <100999677+ajazify@users.noreply.github.com>
2025-05-26 16:27:42 +0530

    Update full-size-alert-dialog-example.dart

--- full-size-alert-dialog-example.dart
+++ full-size-alert-dialog-example.dart
@@ -1,32 +1,59 @@
-  Future<dynamic> fullSizeDialog({required BuildContext context}) {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          insetPadding: EdgeInsets.zero, // Remove default padding
-          child: Material(
-            child: SafeArea(
-              child: Scaffold(
-                appBar: AppBar(
-                  title: const Text('Full Size Dialogue'),
-                  leading: IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: MyDialogueHome());
+  }
+}
+
+class MyDialogueHome extends StatelessWidget {
+  const MyDialogueHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () => fullSizeDialog(context: context),
+          child: Text("Show Full Dialogue Alert!"),
+        ),
+      ),
+    );
+  }
+}
+
+Future<dynamic> fullSizeDialog({required BuildContext context}) {
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return Dialog(
+        insetPadding: EdgeInsets.zero, // Remove default padding
+        child: Material(
+          child: SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.grey.shade200,
+                title: const Text('Full Size Dialogue', style: TextStyle(fontSize: 14)),
+                leading: IconButton(
+                  icon: const Icon(Icons.close, size: 15),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-                body: const Padding(
-                  padding:  EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                     
-                    ],
-                  ),
-                ),
+              ),
+              body: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Center(child: Text("Full Screen Alert Dialogue")),
               ),
             ),
           ),
-        );
-      },
-    );
-  }
+        ),
+      );
+    },
+  );
+}
